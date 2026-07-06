5629c9ee0c8e8076dd0014998090012252ca4080
Logan Norman <lognorman20@gmail.com>
2026-07-06 00:19:32 -0400

    chore(demo): remove duplicate ' 2.swift' files from merge artifact

--- You Are Sigma/Core/API/AppConfig 2.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-enum AppConfig {
-    static var apiBaseURL: URL {
-        if let str = Bundle.main.infoDictionary?["API_BASE_URL"] as? String,
-           let url = URL(string: str) { return url }
-        return URL(string: "http://localhost:3000")!
-    }
-}
--- You Are SigmaUITests/QuickSmokeUITests 2.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-final class QuickSmokeUITests: XCTestCase {
-    func testAppLaunches() {
-        let app = XCUIApplication()
-        app.launch()
-        XCTAssertTrue(app.exists)
-    }
-}
